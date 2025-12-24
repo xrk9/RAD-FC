67cc9fa904aa3aa6113c9d24b29f4b1e2bd3ba0e
r4hu1 <rkgupta30052006@gmail.com>
2025-12-25 00:38:46 +0530

    All done

--- fc_v2/fc_v2.ino
+++ fc_v2/fc_v2.ino
@@ -1,20 +1,21 @@
 // Flight Controller v2 by Rahul Kumar
+// Project RAD-1
 
 // Libraries
-#include <Wire.h>             // for MPU
-#include <RF24.h>             // for NRF24L01
-#include <Servo.h>            // for esc
-#include <math.h>             // for trignometry
+#include <Wire.h>             // for MPU communication
+#include <RF24.h>             // for NRF24L01 communication
+#include <Servo.h>            // for esc communication
+#include <math.h>             // for trignometry and some other functions ( like sqrt )
 #include <stdint.h>           // for standard integers
-#include <Adafruit_BMP280.h>  // for BMP 280
+#include <Adafruit_BMP280.h>  // for BMP 280 communication
 
 // Addresses for I2C
-#define MPU_ADDR 0x68 // MPU address
-#define BMP_ADDR 0x76 // try 0x77 for older bmp 280
+#define MPU_ADDR 0x68 // MPU 6050 Address
+#define BMP_ADDR 0x76 // BMP 280 Address //try 0x77 for older bmp 280
 
 // Arudino Pins
-#define led_g 3   // green led
-#define led_r 4   // red led
+#define led_g 3   // green led pin
+#define led_r 4   // red led pin
 #define ce 7      // CE pin of NRF
 #define csn 8     // CSN pin of NRF
 
@@ -29,17 +30,31 @@
 #define LOOP_1 10000    // Angle Kalman (Accel Reading) and RC input receive timer // 1_000_000 / 100 = 10_000
 #define LOOP_BARO 40000 // ZInertial velocity Kalamn (Baro Reading) // 1_000_000 / 25 = 40_000
 
+// Constant Valuues
+#define KP_T 10 
+#define KP_P 10 
+#define KP_R 10
+#define KP_Y 10
+
+#define KD_T 10
+#define KD_P 10
+#define KD_R 10
+#define KD_Y 10
+
 // Adafruit Barometer Object
 Adafruit_BMP280 bmp;
 
-// Loop 
+// Servo Object
+Servo escFL, escFR, escBL, escBR;
+
+// Loop timers -> to keep track of loop timing
 unsigned long loop_0 = 0, loop_1 = 0, loop_baro = 0;
 
 // Last Loop Timer (micros)
 unsigned long last_0 = 0, last_1 = 0, last_baro = 0; // last time for loop_0, loop_1 and loop_baro respectively at 500, 100, 25Hz respectively
 
-// Servo Object
-Servo escFL, escFR, escBL, escBR;
+// dt time
+double dt_accel = 0.0d;
 
 // Struct Declaration
 struct KalmanA;
@@ -55,33 +70,29 @@ int16_t ringBuzzerFor = 0;              // How long to ring buzzer
 int16_t raw_ax = 0, raw_ay = 0, raw_az = 0; // accel
 int16_t raw_gx = 0, raw_gy = 0, raw_gz = 0; // gyro
 
-// sensor values converted floats
-float ax = 0.0f, ay = 0.0f, az = 0.0f; // accel in g
-float gx = 0.0f, gy = 0.0f, gz = 0.0f; // gyro in deg/s
+// Lat values of IMU (MPU 6050)
+float last_ax = 0.0f, last_ay = 0.0f, last_az = 0.0f;
+float last_gx = 0.0f, last_gy = 0.0f, last_gz = 0.0f;
 
 // Gyro  and Acc Offsets (raw)
 int16_t gx_offset = 0, gy_offset = 0, gz_offset = 0;
 int16_t ax_offset = 137, ay_offset = -181, az_offset = -1346; // update later if needed
 
-// Downward Inertial acceleration and it's Baseline Acceleration
-float accZInertial = 0.0f; 
+// Downward Inertial acceleration base
 float baselineAccZInertial = 0.0f; // The Inertial Acceleration value when vertical velocity = 0m/s, it's not always 1g in our case
+float lastLinearAccZInertial = 0.0f; // last value
 
-// Final Angle and Vertical Velocity
+// Angle and Vertical Velocity
 float rollAngle = 0.0f, pitchAngle = 0.0f;
 float verticalSpeed = 0.0f;
 
 // Barometer
 float baselinePressure = 0.0f; //HPa
 float lastAltitude = 0.0f;
-float alpha = 0.25; // low pass filter
+float lastVelocity = 0.0f;
 
 // Bring Down bool
 bool bringDown = false;
-
-// Flight Controller
-float throttle = 0.0f, pitch = 0.0f, roll = 0.0f, yaw = 0.0f; // esc microsecond values
-
 
 struct Target{
   float pitchAngle;   // pitch angle 
@@ -93,7 +104,7 @@ struct Target{
   float rollAcc;      // roll accel
 
   float yawRate;      // yaw rate
-  float yawAcc;       // yaw accel
+  float yawAcc;       // yaw Acc
 
   float vVelocity;    // vertical velocity
   float Acc;          // vertical acceleration
@@ -140,9 +151,9 @@ KalmanA kalRoll, kalPitch;
 RF24 radio(ce,csn);
 const byte address[5] = {'R', 'A', 'D', '-', '0'};
 
-// Sign inline function
+// Sign function
 inline int sign(float x) {
-  return (x>0.0f) - (x<0.0f);
+  return (x > 0.0f) - (x < 0.0f);
 }
 
 
@@ -180,8 +191,10 @@ void startListening(){
 // if connection lost for more than 15 seconds -> land down the drone;
 void read_receiver() {
 
+  int16_t buzzTimee = ringBuzzerFor;
+
   // ack
-  radio.writeAckPayload(0, &ringBuzzerFor, sizeof(ringBuzzerFor));
+  radio.writeAckPayload(0, &buzzTimee, sizeof(buzzTimee));
   ringBuzzerFor = 0;   // clear after loading
 
   // store data
@@ -192,12 +205,13 @@ void read_receiver() {
   }
 
   unsigned long currentTimeRadio = millis();
+  unsigned long lastTimeRadio = lastReceiveTimeRadio;
 
   // failsafe and bring down
-  if (currentTimeRadio - lastReceiveTimeRadio > 1000) {
+  if (currentTimeRadio - lastTimeRadio > 1000) {
     resetData();
 
-    if (currentTimeRadio - lastReceiveTimeRadio > 15000) {
+    if (currentTimeRadio - lastTimeRadio > 15000) {
       bringDown = true;
     }
   }
@@ -341,9 +355,9 @@ bool readAccelRaw(){
 
   // Read as unsigned and combine to signed int16_t safely
   uint8_t hi, lo;
-  hi = Wire.read(); lo = Wire.read(); raw_gx = (int16_t)((hi << 8) | lo) - ax_offset;
-  hi = Wire.read(); lo = Wire.read(); raw_gy = (int16_t)((hi << 8) | lo) - ay_offset;
-  hi = Wire.read(); lo = Wire.read(); raw_gz = (int16_t)((hi << 8) | lo) - az_offset;
+  hi = Wire.read(); lo = Wire.read(); raw_ax = (int16_t)((hi << 8) | lo) - ax_offset;
+  hi = Wire.read(); lo = Wire.read(); raw_ay = (int16_t)((hi << 8) | lo) - ay_offset;
+  hi = Wire.read(); lo = Wire.read(); raw_az = (int16_t)((hi << 8) | lo) - az_offset;
 
   return true;
 }
@@ -379,9 +393,9 @@ void calibrate() {
   gz_offset = (float)sumGZ / validSamples;
 
   
-  ax = (float)sumAX / validSamples / 8192;
-  ay = (float)sumAY / validSamples / 8192;
-  az = (float)sumAZ / validSamples / 8192;
+  float ax = (float)sumAX / validSamples / 8192;
+  float ay = (float)sumAY / validSamples / 8192;
+  float az = (float)sumAZ / validSamples / 8192;
 
   // Calculate Aproximate angles
   float PitchAngle = atan2f(-ax, sqrtf(ay*ay + az*az)) * 180.0f / PI;
@@ -393,6 +407,13 @@ void calibrate() {
   baselinePressure = bmp.readPressure() / 100;  // HPa
 }
 
+// PT1 filter;
+// [float prev, float curr, float dt, float loop_rate] -> [float filterred_value]
+float pt1(float prev, float input, float dt, float fc){
+    float RC = 1.0f / (2.0f * PI * fc);
+    float alpha = dt / (RC + dt);
+    return prev + alpha * (input - prev);
+}
 
 // Initialte Kalman filter using our Q_anlge, Q_bias, R_measure
 void kalmanInitA(KalmanA &k) {
@@ -510,21 +531,62 @@ void getTarget(){
   
   target.pitchAcc = (float)Pot2 * 12.0f + 48.0f;  // [60, 120]
   target.rollAcc  = (float)Pot2 * 12.0f + 48.0f;  // [60,120]
-  target.yawAcc   = (float)Pot1 * 7.0f + 30.0f;  // [37, 100]
+  target.yawAcc = (float)Pot1 * 60.0f + 40.0f;  // [40, 100]
   target.Acc      = (float)Pot1 * 0.4; // [0.4, 4.0] m/s2
 
+  float last_altitude = lastAltitude;
+
   if(bringDown){
-    if(lastAltitude < 5) target.vVelocity = -0.3;
-    else if (lastAltitude < 1) target.vVelocity = -0.1;
-    else target.vVelocity = -0.2f * lastAltitude;
+    if(last_altitude < 5) target.vVelocity = -0.3;
+    else if (last_altitude < 1) target.vVelocity = -0.1;
+    else target.vVelocity = -0.2f * last_altitude;
     target.Acc = 0.4f;
   }
 
 }
 
+/* ------------ Working ------------------
 
-// 3 parts of get tele, gyro separate, accel separate, and baro separate
+theta_measuure, omega_measure   -> measured by sensors
+theta_target, omega_target      -> wanted theta and omega
+omega_max, theta_max            -> Max values of omega and alpha allowed
 
+Error (e) = theta_target - theta_measure          -> angle error
+omega_cap = sqrt(2 * alpha_max * |e|)             -> Max value of omega so that it stops at target
+omega_out = sign(e) * min(omega_max, omega_cap)   -> what omega we want right now
+
+// We want omega to follow s-curve, that is no jerk
+// We will increment omega little by little
+// omega_last is the omega we had lastly
+d_omega = omega_out - omega_last            -> Difference in desired and current omega
+d_omega_max = alpha_max * dt                -> What max omega change we want
+wrap(d_omega, +-d_omega_max)                -> Wrap d_omega between max desired d_omega
+omega_last = omega_last + d_omega           -> Update our omega
+cover(omega_last, +- omega_max)             -> Conver it between max values
+
+Simple Math 😇
+
+ --------------------------------------- */
+
+
+// Control Loop
+float control_loop(float e, float alpha_max, float omega_max, float omega_last, float dt){
+
+  float omega_cap = sqrt(2.0f * alpha_max * fabs(e) );
+  float omega_out = sign(e) * fminf( omega_cap, omega_max );
+
+  float d_omega = omega_out - omega_last;
+  float max_d_omega = alpha_max * dt;
+
+  if(d_omega > max_d_omega) d_omega = max_d_omega;
+  else if (d_omega < -max_d_omega) d_omega = -max_d_omega;
+
+  float omega = omega_last + d_omega;
+  if(omega > omega_max) omega = omega_max;
+  else if (omega < -omega_max) omega = -omega_max;
+
+  return omega;
+}
 
 
 // clamp valuese in closed range [1035,1965]
@@ -565,7 +627,6 @@ void armESC(){
 }
 
 
-
 void setup(){
   // Serial.begin(9600);
 
@@ -576,7 +637,12 @@ void setup(){
   pinMode(led_r, OUTPUT);   // Red
   digitalWrite(led_r, HIGH);
 
-  startListening();
+  for(int i = 0; i < 10; i++){
+    if(radio.isChipConnected()){
+      startListening();
+      break;
+    }
+  }
 
   bool mpu_ok = false;
   bool bmp_ok = false;
@@ -585,15 +651,14 @@ void setup(){
   for(int i = 0; i < 10; i++){
     if(!mpu_ok) mpu_ok = beginMPU();
     if(!bmp_ok) bmp_ok = beginBMP();
-    if(!radio.isChipConnected()) startListening();
 
-    if(mpu_ok && bmp_ok && radio.isChipConnected()) break;
+    if(mpu_ok && bmp_ok ) break;
     delay(50);
   }
 
   // if still not initialized, raise error
   if(!mpu_ok || !bmp_ok || !radio.isChipConnected()){
-    Serial.print("Sensor Bad");
+    // Serial.print("Sensor Bad");
     
     while(1){
       delay(500);
@@ -605,8 +670,8 @@ void setup(){
 
 
   // --- Initiate Kalman filters ---
-  kalmanInitA(kalRoll);
   kalmanInitA(kalPitch);
+  kalmanInitA(kalRoll);
   kalmanInitB(kalVel);
 
 
@@ -625,7 +690,25 @@ void setup(){
 
   ringBuzzerFor = 1500;
   read_receiver();
+  target.vVelocity = 0.0f;
   delay(1500);          // 1500 delay for buzzer ring
+
+  lastAltitude = bmp.readAltitude(baselinePressure);
+  last_baro = micros();
+  readMPUraw();
+  last_0 = last_1 = micros();
+  
+  // Accel : 4g = 8192 LSB/g
+  last_ax = (float) raw_ax / 8192;
+  last_ay = (float) raw_ay / 8192;
+  last_az = (float) raw_az / 8192;
+
+  // Gyro: 2000dps = 16.4 LSB/(deg/s)
+  last_gx = (float) raw_gx / 16.4;
+  last_gy = (float) raw_gy / 16.4;
+  last_gz = (float) raw_gz / 16.4;
+
+  lastLinearAccZInertial = baselineAccZInertial;
 
   loop_0 = loop_1 = loop_baro = micros();
 }
@@ -633,20 +716,205 @@ void setup(){
 void loop(){
   unsigned long now = micros();
 
+  // 100Hz loop - Radio Read -> get Target, MPU angle Read -> angle kalman, vertical speed prediction
+  if((now - loop_1) >= 0){
+    loop_1 += LOOP_1;
+
+    read_receiver();
+
+    // Kill check
+    if(!data.tSwitch1){
+      escFL.writeMicroseconds(1000);
+      escFR.writeMicroseconds(1000);
+      escBL.writeMicroseconds(1000);
+      escBR.writeMicroseconds(1000);
+      digitalWrite(led_r,HIGH);
+      while(true){
+        read_receiver();
+        if(data.tSwitch1) break;
+      }
+    }
+
+    getTarget();
+
+    readAccelRaw();
+    unsigned long currTime = micros();
+    double dt = (currTime - last_1) / 1000000.0f;
+    last_1 = currTime;
+
+    // Convert raw to physical units
+    // Accel : 4g = 8192 LSB/g
+    float ax = (float) raw_ax / 8192;
+    float ay = (float) raw_ay / 8192;
+    float az = (float) raw_az / 8192;
+
+    if(dt <= 0.03 && dt > 0){
+
+      float filax = pt1(last_ax, ax, dt, 25);
+      float filay = pt1(last_ay, ay, dt, 25);
+      float filaz = pt1(last_az, az, dt, 25);
+
+      last_ax = filax;
+      last_ay = filay;
+      last_az = filaz;
+
+      // Compute accel angles (deg)
+      float rollAng = atan2f(filay, sqrtf(filay*filay + filaz*filaz)) * 180.0f / PI;
+      float pitchAng = atan2f(-filax, sqrtf(filay*filay + filaz*filaz)) * 180.0f / PI;
+      float accZInertial = -sin(pitchAng*(PI/180))*filax+cos(pitchAng*(PI/180))*sin(rollAng*(PI/180))* filay+cos(pitchAng*(PI/180))*cos(rollAng*(PI/180))*filaz;
+      accZInertial = (accZInertial - baselineAccZInertial) * 9.81;
+
+      float fil_accZInertial = pt1(lastLinearAccZInertial, accZInertial, dt, 25);
+
+      // Update Kalman filters
+      rollAngle = kalmanUpdateA(kalRoll, last_gx, rollAng, dt);
+      pitchAngle = kalmanUpdateA(kalPitch, last_gy, pitchAng, dt);
+      verticalSpeed += (fil_accZInertial - kalVel.bias) * dt;
+      dt_accel = dt;
+
+    }else{
+
+      last_ax += 0.557 * (ax - last_ax);
+      last_ay += 0.557 * (ay - last_ay);
+      last_az += 0.557 * (az - last_ax);
+
+    }
+    now = micros();
+  }
+
   // 25Hz loop - Baro Kalman
   if((now-loop_baro) >= 0){
     loop_baro += LOOP_BARO;
 
-    // Read Baro
-    
-  }
+    // Read velocity from barometer
+    float curr = bmp.readAltitude(baselinePressure);
+    unsigned long currentTime = micros();
+    double dt = ( currentTime - last_baro ) / 1000000.0f;
+    last_baro = currentTime;
 
+    if(dt <= 0.1 && dt > 0){
+      float last_alt = lastAltitude;
+      float curr_fil = pt1(last_alt, curr, dt, 3.0f );
+      float vel_raw = (curr_fil - last_alt) / dt;
+      lastAltitude = curr_fil;
+      float vel_fil = pt1(lastVelocity, vel_raw, dt, 6.0f);
+
+      float pitchAng = pitchAngle;
+      float rollAng = rollAngle;
+      float ax = last_ax;
+      float ay = last_ay;
+      float az = last_az;
+
+      float accZInertial = -sin(pitchAng*(PI/180))*ax+cos(pitchAng*(PI/180))*sin(rollAng*(PI/180))* ay+cos(pitchAng*(PI/180))*cos(rollAng*(PI/180))*az;
+      accZInertial = (accZInertial - baselineAccZInertial) * 9.81;
+
+      float fil_accZInertial = pt1(lastLinearAccZInertial, accZInertial, dt, 25);
+
+      verticalSpeed =  kalmanUpdateB(kalVel, fil_accZInertial, vel_fil, dt_accel);
+    }else{
+      lastAltitude += 0.43 * (curr - lastAltitude);
+    }
+    
+    now = micros();
+  }
   
   // Inner 500Hz loop
   if((now - loop_0) >= 0){
     loop_0 += LOOP_0;
 
+    readGyroRaw();  // Read raw
+    unsigned long currTime = micros();
+    double dt = (currTime - last_0) / 1000000.0f;
+    last_0 = currTime;
+    
+    // Convert raw to physical values
+    // Gyro: 2000dps = 16.4 LSB/(deg/s)
+    float gx = (float) raw_gx / 16.4;
+    float gy = (float) raw_gy / 16.4;
+    float gz = (float) raw_gz / 16.4;
 
+    gx = gx - kalRoll.bias;
+    gy = gy - kalPitch.bias;
+
+    if(dt <= 0.006 && dt > 0){
+      float fil_gx = pt1(last_gx, gx, dt, 100);
+      float fil_gy = pt1(last_gy, gy, dt, 100);
+      float fil_gz = pt1(last_gz, gz, dt, 100);
+
+      last_gx = fil_gx;
+      last_gy = fil_gy;
+      last_gz = fil_gz;
+
+      rollAngle += fil_gx * dt;
+      pitchAngle += fil_gy * dt;
+
+      // Flight Controller
+
+      // Pitch
+      float pitch_omega;
+      float pitch_error = target.pitchAngle - pitchAngle;
+      if( fabsf(pitch_error) > 1){
+        pitch_omega = control_loop(pitch_error, target.pitchAcc, target.pitchRate, fil_gx, dt);
+      } else pitch_omega = 0.0f;
+
+      float pitch = (pitch_omega - fil_gx) * KP_P - KD_P * fil_gx;
+      
+      // Roll
+      float roll_omega;
+      float roll_error = target.rollAngle - rollAngle;
+      if( fabsf(roll_error) > 1){
+        roll_omega = control_loop(roll_error, target.rollAcc, target.rollRate, fil_gy, dt);
+      } else roll_omega = 0.0f;
+
+      float roll = (roll_omega - fil_gy) * KP_R - KD_R * fil_gy;
+
+      // Yaw
+      float target_yaw = target.yawRate;
+      float yaw_meas = fil_gz;
+      float yaw_error = target_yaw - yaw_meas;
+      float yaw_cmd;
+      if(fabsf(yaw_error) > 5){
+        float max_error = target.yawAcc * dt;
+        if(yaw_error > max_error) yaw_error = max_error;
+        if(yaw_error < -max_error) yaw_error = -max_error;
+        yaw_cmd = yaw_meas + yaw_error;
+        if(yaw_cmd > target_yaw) yaw_cmd = target_yaw;
+        else if ( yaw_cmd < -target_yaw) yaw_cmd = -target_yaw;
+      } else yaw_cmd = 0.0f;
+      
+      float yaw = KP_Y * (yaw_cmd - fil_gz) - KD_Y * (fil_gz);
+
+      // Vertical Acceleration
+      float target_vz = target.vVelocity;
+      float now = verticalSpeed;
+      float vz_error = target_vz - now;
+      float vz;
+
+      if(fabsf(vz_error) > 0.15) {
+        float max_vz_error = target.Acc * dt;
+        if(vz_error > max_vz_error) vz_error = max_vz_error;
+        if(vz_error < -max_vz_error) vz_error = -max_vz_error;
+        vz = now + vz_error;
+        if(vz > target_vz) vz = target_vz;
+        if(vz < -target_vz) vz = -target_vz;
+      } else vz = 0.0f;
+     
+      float throttle = KP_T * (vz - now) - KD_T * (now);
+
+      
+      // preprating values for escs'
+      escFR.writeMicroseconds(clamp(throttle - roll - pitch - yaw));
+      escBR.writeMicroseconds(clamp(throttle - roll + pitch + yaw));
+      escBL.writeMicroseconds(clamp(throttle + roll + pitch - yaw));
+      escFL.writeMicroseconds(clamp(throttle + roll - pitch + yaw));
+
+
+    }else{
+      last_gx += 0.557 * (gx - last_gx);
+      last_gy += 0.557 * (gy - last_gy);
+      last_gz += 0.557 * (gz - last_gz);
+    }
+    
   }
 }
 
